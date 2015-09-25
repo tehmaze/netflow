b36c9d90ff424edc5135b7dd4335c980d9a53651
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2015-09-25 18:15:30 +0200

    IPFIX docs

---
+++ ipfix/doc.go
@@ -0,0 +1,17 @@
+/*
+Package ipfix implements a decoder for the IP Flow Information Export (IPFIX)
+protocol.
+
+About
+
+Internet Protocol Flow Information Export (IPFIX) is an IETF protocol, as well
+as the name of the IETF working group defining the protocol. It was created
+based on the need for a common, universal standard of export for Internet
+Protocol flow information from routers, probes and other devices that are used
+by mediation systems, accounting/billing systems and network management systems
+to facilitate services such as measurement, accounting and billing.
+
+The IPFIX standard defines how IP flow information is to be formatted and
+transferred from an exporter to a collector.
+*/
+package ipfix
