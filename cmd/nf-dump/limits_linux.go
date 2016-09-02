3a562cb9f7380bcbbd117790906e3d7c852475a3
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2016-09-02 12:49:39 +0200

    Wrong variable

--- cmd/nf-dump/limits_linux.go
+++ cmd/nf-dump/limits_linux.go
@@ -11,10 +11,10 @@ func init() {
 	// Attempt to read the actual limit from procfs and fail silently.
 	b, err := ioutil.ReadFile("/proc/sys/net/core/rmem_max")
 	if err == nil {
-		readBuffer, err = strconv.Atoi(string(b))
+		readSize, err = strconv.Atoi(string(b))
 	}
 	if err != nil {
 		// sysctl net.core.rmem_max -> 212992
-		readBuffer = 212992
+		readSize = 212992
 	}
 }
